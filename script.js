ccbf42fef2d18db6395adeb7e4281e77ac4f6658
dudavtra <vitoria.souza.eduarda@escola.pr.gov.br>
2025-09-12 10:59:46 -0300

    ok

---
+++ script.js
@@ -0,0 +1,19 @@
+const avanca=document.querySelectorAll('.btn-proximo')
+const reiniciarbtn=document.getElementById('btn-reiniciar');
+
+
+avanca.forEach(button=> {
+    button.addEventListener('click',fuction(){
+        const atual=document.querySelector('ativo');
+        const proximoPasso='passo-'+this.getAttribue('data-proximo')
+        
+        atual.classList.remove('ativo');
+        const proximoElemento=document.getElementById(proximoPasso);
+
+        if(proximoElemento){
+            proximoElemento.classList.add('ativo');
+        } else{
+            console.error(`Elemento com ID "${proximo passo}"nao encontrado.`);
+        }
+    })
+})
